53a73a5f0e161270ae26cf3dee32ff071ca46ad8
Sveinung Berntsen <sveinung.berntsen@epinova.no>
2025-03-24 22:48:44 +0100

    Endringer

--- wp-content/themes/blankslate/inc/hero-manual.php
+++ wp-content/themes/blankslate/inc/hero-manual.php
@@ -5,13 +5,13 @@
             <?php the_title(); ?>
         </h1>
         <?php if ( has_excerpt() ) {     
-            echo "<div class='hero__ingress'>" . get_the_excerpt() . " <a href='#'>Les mer om prosjektet</a>.</div>";
+            echo "<div class='hero__ingress'>" . get_the_excerpt() . " <a href='https://humankind.no/prosjekt-shelter/'>Les mer om prosjektet</a>.</div>";
             }
         ?>
 
         <div class="qr-code">
-            <img src="http://localhost:8888/humankind/wp-content/uploads/2025/03/QR-code_HUMAN_KIND-BEBOTH656680.png" alt="QR code for Vipps" class="qr-code__image">
-            <div class="qr-code__text">Skann QR-koden eller vipps til 65 66 80 for å gi. <a href="#">Andre alternativer</a></div>
+            <img src="https://humankind.no/wp-content/uploads/2025/03/qr-vipps-engang.png" alt="QR code for Vipps" class="qr-code__image">
+            <div class="qr-code__text">Skann QR-koden eller vipps til 65 66 80 for å gi. <a href="https://humankind.no/vaer-med-a-hjelpe/">Andre alternativer</a>.</div>
         </div>
     </div>
 
